20608a14899e19292ed21895a5caa6ab7c5748bf
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2024-01-05 13:25:09 +0100

    Learning about javascript loops.

---
+++ loops/js/script.js
@@ -0,0 +1,70 @@
+// loop is for repeating!!
+
+// for (initialization; condition; increment;) {
+
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+            // 0         1       2       3
+const cars = ["Tesla", "BMW", "Volvo", "VW"];
+
+console.log(cars[0]);
+console.log(cars[3]);
+console.log(cars.length);
+
+const totalNumberOfCars = cars.length;
+
+for (let i = 0; i < totalNumberOfCars; i++) {
+    console.log(cars[i]);
+}
+
+const fruits = ["Apple", "Banana", "Cherry"];
+
+const totalNumberOfFruits = fruits.length;
+
+
+for (let i = 0; i < totalNumberOfFruits; i++) {
+    console.log(fruits[i]);
+}
+
+// 0 // good
+// 1 
+// 2
+// 3
+// 4
+
+// i = 0
+// true
+// i = 1
+
+// i = 1
+// true
+// console.log (1)
+// 1 + 1 = 2
+
+// i = 2
+// true
+// console.log (2)
+// increment 2 + 1 
+// i = 3
+
+// true
+// 3 + 1
+// console.log (3)
+// i = 4
+
+// true
+// 4 + 1
+// console.log (4)
+
+// i = 5
+// false
+
+// it goes like this until the condition is false
