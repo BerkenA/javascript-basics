9d4f933f3d5c4d7a1f0abf2e63fcc9d1169347d6
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2024-01-15 15:05:16 +0100

    Did a lesson task in javascript.

--- advanced/js/script.js
+++ advanced/js/script.js
@@ -18,3 +18,36 @@ function isEmpty(obj){
 
 const isMyObject = isEmpty(user);
 console.log(isMyObject);
+
+// Q3
+
+// Given an array of numbers: [1,2,31,24,-67,5,3,555,79,-4,21,33,89,-90].
+// 1) Find the maximum and minimum value of the array.
+// 2) Write all positive and even numbers into another array.
+// Display the results on the screen.
+
+const numberArray = [1,2,31,24,-67,5,3,555,79,-4,21,33,89,-90];
+const odds = numberArray.filter((num) => num % 2 === 1);
+console.log(odds);
+
+numberArray.sort(function (a , b){
+    return a - b
+});
+
+console.log(numberArray);
+console.log(numberArray[0]);
+console.log(numberArray[numberArray.length - 1]);
+
+function countingEvenOdd(arr, arr_size){
+    let even_count = 0;
+    let odd_count = 0;
+    for(let i=0; i < arr_size; i++){
+        if(arr[i] & 1 == 1)
+        odd_count++;
+        else
+        even_count++;
+    }
+    document.write("number of even element = " + even_count + "<br>" + "number of odd elements = " + odd_count);
+}
+
+console.log(countingEvenOdd);
